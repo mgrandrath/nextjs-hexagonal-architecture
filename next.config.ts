c5a0e8d6859e82ee2bcfbbaab388b160e78b343b
Martin Grandrath <martin.grandrath@codecentric.de>
2025-12-31 14:20:26 +0100

    feat: allow pages to be cached for 5 minutes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/spaceships/:id",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=600",
+          },
+        ],
+      },
+      {
+        source: "/spaceships",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=600",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
